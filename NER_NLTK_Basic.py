9d364145a4444b6c1a9918eb9fb7a32010081f29
sandeepz441 <51243067+sandeepz441@users.noreply.github.com>
2019-07-11 11:39:03 -0500

    Update NER_NLTK_Basic.py

--- NER_NLTK_Basic.py
+++ NER_NLTK_Basic.py
@@ -1,1 +1,41 @@
 
+import nltk
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+ex = 'European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices'
+
+def preprocess(sent):
+    sent = nltk.word_tokenize(sent)
+    sent = nltk.pos_tag(sent)
+    return sent
+  
+sent = preprocess(ex)
+sent
+
+
+pattern = 'NP: {<DT>?<JJ>*<NN>}'
+
+cp = nltk.RegexpParser(pattern)
+cs = cp.parse(sent)
+print(cs)
+
+
+
+
+from nltk.chunk import conlltags2tree, tree2conlltags
+from pprint import pprint
+iob_tagged = tree2conlltags(cs)
+pprint(iob_tagged)
+
+ne_tree = nltk.ne_chunk(pos_tag(word_tokenize(ex)))
+print(ne_tree)
+
